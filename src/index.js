656c0dd0fc264bd881378cff896fa928fcecc6f9
zyvora <zenithxnecxus@gmail.com>
2026-05-24 20:15:18 +0700

    Remove unused network command imports
    Removed unused network command imports.

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ import { createSpinner } from './utils/spinner.js';
 import { showHelp }                            from './commands/help.js';
 import { cmdProvider, cmdSetKey, cmdStatus }   from './commands/provider.js';
 import { cmdRead, cmdExec }                    from './commands/read.js';
-import { cmdWhois, cmdDns, cmdPort, cmdSearch } from './commands/network.js';
 import { cmdRun }                              from './commands/runner.js';
 import { cmdDebug, cmdFeature }                from './commands/ai_tools.js';
 
